955ad550ab9dde4ac16e84789a6437b5f0f8dc04
Tabishkhan18 <tabishkhan1811@gmail.com>
2024-04-06 13:17:11 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='text-center py-4 text-slate-800 font-bold'>
+        Copyrights &copy;2024 TechTabish. All Rights Reserved.
+    </footer>
+  )
+}
+
+export default Footer
